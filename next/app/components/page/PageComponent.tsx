70a3448d54170857435145a722f4bb9207538807
Richard Imaoka <richard.e.imaoka@gmail.com>
2024-03-02 23:41:29 +0900

    title

--- next/app/components/page/PageComponent.tsx
+++ next/app/components/page/PageComponent.tsx
@@ -1,23 +1,11 @@
-"use client";
-
-import { useEffect, useRef } from "react";
 import styles from "./PageComponent.module.css";
 
 interface Props {}
 
 export function PageComponent(props: Props) {
-  const ref = useRef<HTMLDivElement>(null);
-
-  function buttonCallback() {
-    if (ref.current) {
-      ref.current.requestFullscreen();
-    }
-  }
-
   return (
-    <div ref={ref} className={styles.component}>
-      page
-      <button onClick={buttonCallback}>full screeen</button>
+    <div className={styles.component}>
+      <h1>Next.jsでドキュメント・サイトのリアルタイム検索ボックスを実装する</h1>
     </div>
   );
 }
